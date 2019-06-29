9420d9c60c427e6158a40815b1d2442c855ee759
Seyed Amin Esmaeily <s.amin.esmaeily@gmail.com>
2019-06-29 23:15:41 +0430

    Test project added
    All Statement types added to TryCatch code fix provider

---
+++ CheckedException.Test.NetCore/MainTest.cs
@@ -0,0 +1,36 @@
+﻿using CheckedException.Core;
+using System;
+
+namespace CheckedException.Test.NetCore
+{
+    public class MainTest
+    {
+        public void MainMethod()
+        {
+            MethodWithoutReturnValue(); // Expression Statement
+
+            Console.WriteLine(MethodWithReturnValue()); // Expression Statement
+
+            var value1 = MethodWithReturnValue(); // Local Declaration Statement
+
+            if (MethodWithReturnValue() == 10) // If Statement
+            {
+            }
+
+            while(MethodWithReturnValue() == 5) // While Statement
+            { }
+        }
+
+        [ThrowsException(typeof(InvalidOperationException))]
+        private void MethodWithoutReturnValue()
+        {
+
+        }
+
+        [ThrowsException(typeof(ArgumentException))]
+        private int MethodWithReturnValue()
+        {
+            return 10;
+        }
+    }
+}
